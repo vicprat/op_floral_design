c740cde0ef963d40fa4cf28f885005496f1e13e8
Victor Prado <vicknan0182@gmail.com>
2021-04-11 22:06:47 -0500

    Nav bar

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,10 +2,11 @@ import styled from 'styled-components';
 
 export const HeaderWrapper = styled.div`
          display: flex;
-         max-width: 1200px;
+         width: 100%;
+         background: #111;
          align-items: center;
          margin: 0 auto;
-         padding: 10px 20px;
+         padding: 10px 10%;
          box-sizing: border-box;
          flex-wrap: wrap;
        `;
