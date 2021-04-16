e36ae5ed39446eedf2dd8144636dc9aed183ddec
Victor Prado <vicknan0182@gmail.com>
2021-04-16 12:29:31 -0500

    navbar

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,15 +1,14 @@
 import styled from 'styled-components';
 
 export const HeaderWrapper = styled.div`
-         position: fixed;
-         display: flex;
-         width: 100%;
-         background: #aaa;
-         align-items: center;
-         justify-content: space-around;
-         padding: 10px auto;
-         box-sizing: border-box;
-         flex-wrap: wrap;
-         z-index: 100;
-       `;
+  display: flex;
+  width: 100%;
+  background: #aaa;
+  align-items: center;
+  justify-content: space-around;
+  padding: 10px auto;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  z-index: 100;
+`;
 
