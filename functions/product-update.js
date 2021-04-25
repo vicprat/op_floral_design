f1aaf70cddf61e48d0b62075cf326fee5b34397c
Victor Prado <vicknan0182@gmail.com>
2021-04-25 18:54:52 -0500

    porduct update funcion part 2

--- functions/product-update.js
+++ functions/product-update.js
@@ -1,6 +1,26 @@
+const faunadb = require('faunadb');
+const verifyWebhookIntegrity = require('shopify-verify-webhook');
+
+const q = faunadb.query;
+
+const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SECRET
+})
+
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: 'Hello world',
-  });
+    const isValid = verifyWebhookIntegrity(process.env.SHOPIFY_WEBHOOK_KEY, event.headers['x-shopify-hmac-sha256'], event.body);
+
+    if(isValid){
+        callback(null, {
+            statusCode: 200,
+            body: 'Hello world',
+          });
+    }else{
+        callback(null, {
+            statusCode: 403,
+            body: 'Error',
+          });
+    }
+
+  
 };
