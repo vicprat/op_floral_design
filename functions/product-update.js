d96d2289288bc3a7090b86bded07ab6f47fe6360
Victor Prado <vicknan0182@gmail.com>
2021-04-25 19:40:01 -0500

    product update function part 3

--- functions/product-update.js
+++ functions/product-update.js
@@ -1,5 +1,6 @@
 const faunadb = require('faunadb');
 const verifyWebhookIntegrity = require('shopify-verify-webhook');
+const axios = require('axios');
 
 const q = faunadb.query;
 
@@ -11,9 +12,50 @@ exports.handler = function (event, context, callback) {
     const isValid = verifyWebhookIntegrity(process.env.SHOPIFY_WEBHOOK_KEY, event.headers['x-shopify-hmac-sha256'], event.body);
 
     if(isValid){
-        callback(null, {
-            statusCode: 200,
-            body: 'Hello world',
+        const body = JSON.parse(event.body);
+        const { id } = body;
+        delete body.updated_at;
+        body.variants.forEach(variant => {
+          delete variant.updated_at;
+          delete variant.inventory_quantity;
+          delete variant.old_inventory_quantity;
+        });
+
+        const bodyString = JSON.stringify(body);
+
+        client
+          .query(q.Get(q.Match(q.Index('product_by_id'), id)))
+          .then(result => {
+            if (result.data.product !== bodyString) {
+              client
+                .query(
+                  q.Update(result.ref, {
+                    data: { product: bodyString },
+                  })
+                )
+                .then(() => {
+                  // call rebuild
+                  axios.post(process.env.NETLIFY_BUILD_URL);
+                })
+                .catch(e => {
+                  console.log('error updating product: ', e);
+                });
+            }
+          })
+          .catch(() => {
+            client
+              .query(
+                q.Create(q.Collection('products'), {
+                  data: { id, product: bodyString },
+                })
+              )
+              .then(() => {
+                //call rebuild
+                axios.post(process.env.NETLIFY_BUILD_URL);
+              })
+              .catch(e => {
+                console.log('error adding to db: ', e);
+              });
           });
     }else{
         callback(null, {
